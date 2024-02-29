e1b7a9d61424a88d47e21fb315671146da9d15a7
Zita Cheng <zitacheng@zitas-air.home>
2024-02-29 20:44:13 +0100

    about

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './assets/logo.png';
 import amapops1 from './assets/amapops1.png';
 import amapops2 from './assets/amapops2.png';
+import google from './assets/google.png';
+import apple from './assets/apple.png';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
@@ -27,19 +29,26 @@ function App() {
           </Navbar.Brand>
           <Nav className="">
             <Nav.Link className="menuItem" href="#home">À propos</Nav.Link>
-            <Nav.Link className="menuItem"href="#features">Features</Nav.Link>
+            <Nav.Link className="menuItem"href="#features">Comment ça marche</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <Row className="mx-0">
+      <Row className="mx-0 py-4 aboutRow">
         <Col md={3}>
-          <Image src={amapops1} />
+          <Image className="mockup" src={amapops1} />
         </Col>
         <Col md={3}>
-          <Image src={amapops1} />
+          <Image className="mockup" src={amapops2} />
         </Col>
-        <Col md={6}>
-          <p>Hello</p>
+        <Col md={6} className="colDesc">
+          <div>
+            <h3 className="align-middle desc">AMAPOP</h3>
+            <p className="align-middle desc">L'application parfait pour faire des échanges de popmart</p>
+            <Row className="mx-0 justify-content-center">
+              <Image className="appIcon" src={apple} />
+              <Image className="appIcon" src={google} />
+            </Row>
+          </div>
         </Col>
       </Row>
     </div>
